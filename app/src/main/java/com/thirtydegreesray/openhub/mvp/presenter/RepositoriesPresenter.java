21a2cb6a7c75b5f43fc334e78b199ce7cc5a72a9
manhp <phammanh029@gmail.com>
2019-10-19 23:35:14 +0700

    - try to make trending repos work

--- app/src/main/java/com/thirtydegreesray/openhub/mvp/presenter/RepositoriesPresenter.java
+++ app/src/main/java/com/thirtydegreesray/openhub/mvp/presenter/RepositoriesPresenter.java
@@ -452,9 +452,17 @@ public class RepositoriesPresenter extends BasePagerPresenter<IRepositoriesContr
             }
         };
 
-        generalRxHttpExecute(forceNetWork -> getGitHubWebPageService()
-                        .getTrendingRepos(forceNetWork, language.getSlug(), since.name()),
-                httpObserver, !isReload);
+        String filter = language.getSlug();
+        if(filter != "all"){
+            generalRxHttpExecute(forceNetWork -> getGitHubWebPageService()
+                            .getTrendingRepos(forceNetWork, filter,   since.name()),
+                    httpObserver, !isReload);
+        }else {
+
+            generalRxHttpExecute(forceNetWork -> getGitHubWebPageService()
+                            .getTrendingReposAll(forceNetWork, since.name()),
+                    httpObserver, !isReload);
+        }
     }
 
     private void parseTrendingPageData(String page){
@@ -463,7 +471,7 @@ public class RepositoriesPresenter extends BasePagerPresenter<IRepositoriesContr
                     ArrayList<Repository> repos = new ArrayList<>();
                     try {
                         Document doc = Jsoup.parse(s, AppConfig.GITHUB_BASE_URL);
-                        Elements elements = doc.getElementsByClass("col-12 d-block width-full py-4 border-bottom");
+                        Elements elements = doc.getElementsByClass("Box-row");
                         if(elements.size() != 0){
                             for (Element element : elements) {
                                 try{
@@ -498,12 +506,12 @@ public class RepositoriesPresenter extends BasePagerPresenter<IRepositoriesContr
     }
 
     private Repository parseTrendingRepositoryData(Element element) throws Exception{
-        String fullName = element.select("div > h3 > a").attr("href");
+        String fullName = element.select("h1 > a").attr("href");
         fullName = fullName.substring(1);
         String owner = fullName.substring(0, fullName.lastIndexOf("/"));
         String repoName = fullName.substring(fullName.lastIndexOf("/") + 1);
 
-        Element descElement = element.select("div > p").first();
+        Element descElement = element.select("p").first();
         StringBuilder desc = new StringBuilder("");
         for(TextNode textNode : descElement.textNodes()){
             desc.append(textNode.getWholeText());
